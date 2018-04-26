6876c723f31159cc89df0d3a3b088447d55caa02
Tanim <tanimcseku@hotmail.com>
2018-04-26 14:57:17 +0600

    image download helper

--- app/src/main/java/lct/mysymphony/Activity/MainActivity.java
+++ app/src/main/java/lct/mysymphony/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,10 +12,28 @@ import com.bumptech.glide.Glide;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import lct.mysymphony.R;
+import lct.mysymphony.helper.DataHelper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    private DataHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +47,60 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.IV);
         Glide.with(this).load(R.drawable.symphoni).into(imageView);
+
+        dbHelper=new DataHelper(this);
+
+        this.downloadImage();
     }
+
+    public void downloadImage(){
+        Log.i("downloadImage","Inside download image");
+        BackTask bt=new BackTask();
+        String imgURL="http://paimages.prothom-alo.com/contents/cache/images/640x360x1/uploads/media/2018/04/22/0055799d71293e70a941a8b7e8861b0b-5adc299d79982.jpg";
+        if(!imgURL.trim().equals("")){
+            bt.execute(imgURL);
+        }
+
+    }
+
+    // AsynnTask to run download an image in background
+    private class BackTask extends AsyncTask<String,Void,Bitmap>{
+        TextView tv;
+        protected void onPreExecute(){
+            Log.i("Donwload","Downloading the image. Please wait...");
+        }
+        protected Bitmap doInBackground(String...params){
+            Bitmap bitmap=null;
+            try {
+                // Download the image
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream is = connection.getInputStream();
+                // Decode image to get smaller image to save memory
+                final BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = false;
+                options.inSampleSize=4;
+                bitmap = BitmapFactory.decodeStream(is,null, options);
+                is.close();
+            }
+            catch(IOException e){
+                return null;
+            }
+            return bitmap;
+        }
+        protected void onPostExecute(Bitmap result){
+            //tv.setVisibility(TextView.GONE);
+            // Insert bitmap to the database
+            dbHelper.insertBitmap(result);
+            //ImageView imgView=(ImageView)findViewById(R.id.imgview);
+            // Read the first image from database and show it in ImageView
+            //imgView.setImageBitmap(dbHelper.getBitmap(1));
+            Log.i("downlaoded image", dbHelper.getBitmap(1).toString());
+        }
+    }
+
 
     public void startSignUpActivity(View view) {
 
