05ec13d19971be5e800c046e22f0c0fc3e9515b1
MultiplexerLab <multiplexer.lab@gmail.com>
2018-04-26 11:56:10 +0600

    Firebase File

---
+++ app/src/main/java/lct/mysymphony/Notification/MyFirebaseInstanceIDService.java
@@ -0,0 +1,19 @@
+package lct.mysymphony.Notification;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        Log.e(TAG, "onTokenRefresh: " + refreshedToken);
+    }
+
+}
