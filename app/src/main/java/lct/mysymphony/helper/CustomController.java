758dc2e3db1442a368b5b0cc2ecdb8a1b2597291
Farhandroid <farhantanvir65@gmail.com>
2018-05-21 02:27:48 +0600

    Audio and video download added...

---
+++ app/src/main/java/lct/mysymphony/helper/CustomController.java
@@ -0,0 +1,44 @@
+package lct.mysymphony.helper;
+
+import android.content.Context;
+import android.media.session.MediaController;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class CustomController extends android.widget.MediaController{
+    Context context;
+    public CustomController(Context context, View anchor) {
+        super(context);
+        this.context=context;
+        setAnchorView(anchor);
+    }
+
+    @Override
+    public void setAnchorView(View view)     {
+        Button fullScreen = new Button(context);
+        fullScreen.setText("FullScreen");
+        Log.e("media controller","Set anchorView");
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(view.getWidth(), 0, 5, 20);
+        params.gravity =  Gravity.RIGHT;
+        addView(fullScreen, params);
+
+        fullScreen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Log.e("media controller","full screen onclick");
+
+               /* Intent i = new Intent("xyxyxyxhx");
+
+                context.sendBroadcast(i);*/
+
+            }
+        });
+    }
+}
